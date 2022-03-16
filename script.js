5c69d9c916f0ab2b558be5d8e9e900cd3c004a76
Tim <tmetcalfe89@gmail.com>
2022-03-16 15:11:32 -0700

    Send request off to server

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 (function () {
   const pokenameForm = document.getElementById("pokename");
 
+  async function getPokemonData(pokename) {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokename}`
+      );
+      if (response.ok) {
+        const pokedata = await response.json();
+        return pokedata;
+      }
+      return null;
+    } catch (error) {
+      return {
+        error: error,
+      };
+    }
+  }
+
   function addEventListeners() {
-    pokenameForm.addEventListener("submit", (e) => {
+    pokenameForm.addEventListener("submit", async (e) => {
       e.preventDefault();
       const formData = new FormData(e.target);
-      console.log(formData.get("pokename"));
+      const pokename = formData.get("pokename");
+
+      const data = await getPokemonData(pokename);
+      if (data === null) {
+        console.log("Pokemon not found.");
+        return;
+      }
+      if (data.error) {
+        console.error(error);
+        return;
+      }
+      console.log(data);
     });
   }
 
