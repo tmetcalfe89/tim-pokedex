2409fa9984198eaacfafad8049d690bc72544dfa
Tim <tmetcalfe89@gmail.com>
2022-03-17 18:26:11 -0700

    Send request off to server

--- src/App.js
+++ src/App.js
@@ -3,9 +3,35 @@ import React, { useState } from "react";
 function App() {
   const [pokename, setPokename] = useState("");
 
-  function handlePokenameSubmit(e) {
+  async function getPokemonData(pokename) {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokename}`
+      );
+      if (response.ok) {
+        const pokedata = await response.json();
+        return pokedata;
+      }
+      return null;
+    } catch (error) {
+      return {
+        error: error,
+      };
+    }
+  }
+
+  async function handlePokenameSubmit(e) {
     e.preventDefault();
-    console.log(pokename);
+    const data = await getPokemonData(pokename);
+    if (data === null) {
+      console.log("Pokemon not found.");
+      return;
+    }
+    if (data.error) {
+      console.error(data.error);
+      return;
+    }
+    console.log(data);
   }
 
   function handlePokenameUpdate(e) {
