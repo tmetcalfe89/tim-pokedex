a5a7858a544acfeb5b9ee33639b29494bd7da7f0
Tim <tmetcalfe89@gmail.com>
2022-03-17 22:13:49 -0700

    Added convenience function for capitalization

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
 function PokemonCard({ name, image, types }) {
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>{capitalizeWord(name)}</h1>
       <img src={image} />
       <div>
         {types.map((type) => (
-          <span>{type}</span>
+          <span>{capitalizeWord(type)}</span>
         ))}
       </div>
     </div>
