28f11ee4b6f1053c07f0d60713a67d91e37cd2ac
Tim <tmetcalfe89@gmail.com>
2022-03-17 18:24:00 -0700

    Moved input into form, and added submit handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+
 function App() {
+  const [pokename, setPokename] = useState("");
+
+  function handlePokenameSubmit(e) {
+    e.preventDefault();
+    console.log(pokename);
+  }
+
+  function handlePokenameUpdate(e) {
+    setPokename(e.target.value);
+  }
+
   return (
     <div>
-      <input />
+      <form onSubmit={handlePokenameSubmit}>
+        <input value={pokename} onChange={handlePokenameUpdate} />
+      </form>
       <div></div> {/* This will become our pokelist. */}
     </div>
   );
